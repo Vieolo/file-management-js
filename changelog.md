7126e4b16a08136581cf20a85b49d2d548681860
Ramtin Abadi <ramtin.abadi@gmail.com>
2021-10-26 17:35:45 +0530

    Version Bump to 0.2.1

--- changelog.md
+++ changelog.md
@@ -1,2 +1,6 @@
 # Change Log
 
+## 0.2.1 (2021-10-26)
+
+- Added `generateBlob` function
+- Added `downloadBlob` function
---
+++ dist/download/download.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Receives a blob and downloads it via the clinet's browser.
+ * @param blob The blob to be downloaded
+ * @param fileName The name of the file to be downloaded
+ */
+export declare function downloadBlob(blob: Blob, fileName: string): Promise<void>;
+/**
+ * Receives multiple blobs and download them inside a ZIP file.
+ * @param blob The list of blobs and the name of each
+ * @param fileName The name of the ZIP file
+ */
+export declare function downloadBlob(blob: {
+    blob: Blob;
+    blobName: string;
+}[], fileName: string): Promise<void>;
